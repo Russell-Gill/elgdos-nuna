5f421d1a861922ed1f0a84f6500589af3b807176
Russell Gill <rgill0030@gmail.com>
2023-11-11 02:23:38 +0000

    I put both too much time, and not enough into this

--- src/App.js
+++ src/App.js
@@ -5,19 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <title>Eldogs Nuna?</title>
       </header>
+      <div className='full-viewport-centered-white-text-background'>
+        <h1 className='center-text-in-middle text-drop-shadow'>Ekki Enn</h1>
+      </div>
     </div>
   );
 }
